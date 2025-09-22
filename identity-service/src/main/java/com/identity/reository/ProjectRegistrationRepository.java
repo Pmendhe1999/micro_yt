cdc77426e68d806d3fda9401dc0508589255d8cb
pmendhe-at-808494043997 <prasad.mendhe@ethosh.com>
2025-09-22 16:16:47 +0530

    changes for removing of project registration controller
    changes for removing of project registration controller

--- identity-service/src/main/java/com/identity/reository/ProjectRegistrationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.identity.reository;
-
-import com.identity.entity.ProjectRegistrationMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRegistrationRepository extends JpaRepository<ProjectRegistrationMaster, Long> {
-    boolean existsByName(String name);
-}
